f223f66190a87793f07d59d32e7b373883e2f276
Sagarikac310 <sagarikac310@gmail.com>
2019-05-28 12:55:01 +0530

    homepage changes

--- sconad1/lib/screens/home.dart
+++ sconad1/lib/screens/home.dart
@@ -25,50 +25,97 @@ class _PageHomeState extends State<PageHome> {
           title: new Text(widget.title),
         ),
         body: new Container(
-          child: new Stack(
-            children: <Widget>[
-              FractionalTranslation(
-                translation: Offset(0.6, -0.6),
-                child: Transform.rotate(
-                  angle: 5.5,
-                  child: Container(
-                    height: 200.0,
-                    width: 350.0,
-                    decoration: BoxDecoration(
-                        color: Colors.lightBlueAccent[100].withOpacity(0.25),
-                        borderRadius: BorderRadius.circular(10.0)),
+          child: SingleChildScrollView(
+            child: new Column(
+              children: <Widget>[
+                FractionalTranslation(
+                  translation: Offset(0.6, -0.6),
+                  child: Transform.rotate(
+                    angle: 5.0,
+                    child: Container(
+                      height: 100.0,
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                          color: Colors.lightBlueAccent[100].withOpacity(0.25),
+                          borderRadius: BorderRadius.circular(20.0)),
+                    ),
                   ),
                 ),
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(padding: EdgeInsets.only(left: uniWidth / 8)),
-                  Column(
+                Column(
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(padding: EdgeInsets.only(left: uniWidth / 8,top:uniHeight/8)),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              'Sconad builds',
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: uniWidth / 22,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              'your',
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: uniWidth / 22,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              'imagination',
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: uniWidth / 22,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ],
+                        ),
+                        Container(
+                            height: 100.0,
+                            width: 100.0,
+                            child: Image.asset("assets/sconadhome1.png",height: 1000.0,width: 1000.0,))
+                      ],
+                    ),
+
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(
-                        'Sconad Builds your',
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: uniWidth / 20,
-                            fontWeight: FontWeight.w700),
+                      Padding(padding: EdgeInsets.only(left: uniWidth / 10,top:uniHeight/8)),
+                      Column(
+                        children: <Widget>[
+                          Padding(
+
+                            child: Container(
+                                height: 100.0,
+                                width: 100.0,
+
+                          ),
+
+                          Text(
+                            'We are an agency of',
+                            style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: uniWidth / 22,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            'Digital Marketing',
+                            style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: uniWidth / 22,
+                                fontWeight: FontWeight.w700),
+                          )
+                        ],
                       ),
-                      Text(
-                        'Imagination',
-                        style: TextStyle(
-                            color: Colors.blue,
-                            fontSize: uniWidth / 18,
-                            fontWeight: FontWeight.w700),
-                      ),
+
                     ],
-                  ),
-                  Container(
-                      height: 100.0,
-                      width: 100.0,
-                      child: Image.asset("assets/Mac.jpg"))
-                ],
-              ),
-            ],
+                  )
+                  ],
+                ),
+              ],
+            ),
           ),
         ));
   }
